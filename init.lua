33a4f2c9226a64c0e573552104fc3bbdeb97e132
Thomas Lock <email@peb.si>
2017-01-16 16:30:14 +0000

    add printing and retreival

--- init.lua
+++ init.lua
@@ -2,3 +2,21 @@ AddCSLuaFile( "cl_init.lua" ) -- Make sure clientside
 AddCSLuaFile( "shared.lua" )  -- and shared scripts are sent.
 
 include('shared.lua')
+
+function ENT:Use( activator, caller )
+  if IsValid(caller) and caller:IsPlayer() then
+
+    local amount = self:GetAmount()
+
+		if amount > 0 then
+	    caller:AddMoney(amount)
+	    caller:SaveMoney()
+	    caller:PrintMessage(HUD_PRINTCENTER, "Got $"..amount)
+			amount = 0
+      self:SetAmount(0)
+			text = ""
+		else
+			text = "no"
+		end
+  end
+end
--- shared.lua
+++ shared.lua
@@ -1,36 +1,56 @@
 ENT.Type = "anim"
 ENT.Base = "base_gmodentity"
 
-ENT.PrintName= "Bath"
-ENT.Author= "Mruno"
-ENT.Contact= "Via steam"
-ENT.Purpose= "Exemplar material"
-ENT.Instructions= "Use wisely"
+ENT.PrintName= "Money Printer"
+ENT.Author= "Pebsie"
 ENT.Spawnable = true
 ENT.AdminSpawnable = false
 
+ENT.moneyAmount = 0
+text = ""
+
+function ENT:SetupDataTables()
+	self:NetworkVar("Int", 0, "Amount")
+end
 
 function ENT:Initialize()
-
 	self:SetModel( "models/props_c17/consolebox01a.mdl" )
 	self:PhysicsInit( SOLID_VPHYSICS )      -- Make us work with physics,
 	self:SetMoveType( MOVETYPE_VPHYSICS )   -- after all, gmod is a physics
 	self:SetSolid( SOLID_VPHYSICS )         -- Toolbox
 
+	self:SetUseType(SIMPLE_USE)
+
+
   local phys = self:GetPhysicsObject()
 	if (phys:IsValid()) then
 		phys:Wake()
 	end
+	timer.Create( "addmoney", 3, 0, function() local amount = self:GetAmount() amount = amount + math.random(1,2) self:SetAmount(amount) end)
 end
 
-function ENT:Use( activator, caller )
-  if IsValid(caller) and caller:IsPlayer() then
-    caller:AddMoney(100)
-    caller:SaveMoney()
-    caller:PrintMessage(HUD_PRINTCENTER, "Got $100!")
-  end
-end
+
+
+function ENT:Draw()
+	self.Entity:DrawModel()
+
+	local Pos = self:GetPos()
+	local Ang = self:GetAngles()
+
+	local amount = self:GetAmount()
+
+	surface.SetFont("HUDNumber5")
+	text = "Printer: $"..amount
+	local TextWidth = surface.GetTextSize(text)
+
+
+	Ang:RotateAroundAxis(Ang:Up(), 90)
+
+	cam.Start3D2D(Pos + Ang:Up() * 11.5, Ang, 0.11)
+		 draw.WordBox(2, -TextWidth * 0.5, -30, text, "HUDNumber5", Color(140, 0, 0, 100), Color(255, 255, 255, 255))
+	cam.End3D2D()
+ end
 
 function ENT:Think()
-    -- We don't need to think, we are just a prop after all!
+
 end
