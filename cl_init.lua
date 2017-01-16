fee3d02d02dd8ec7801676aa656f67baaa55a3f8
Thomas Lock <email@peb.si>
2017-01-16 18:36:46 +0000

    fix alpha bug

--- cl_init.lua
+++ cl_init.lua
@@ -5,8 +5,7 @@ hook.Add("HUDPaint", "DrawMoney", function()
 	local eye = LocalPlayer():GetEyeTrace()
 
 	if eye.Entity:GetClass() == "f2s-printer" then
-		viewAlpha = viewAlpha + 100
-		draw.SimpleText("Money Print (Contains $"..eye.Entity:GetPrintAmount()..")", "CenterPrintText", ScrW()/2, ScrH()/2, Color(255,0,0),1,1)
+		draw.SimpleText("Money Printer (Contains $"..eye.Entity:GetPrintAmount()..")", "CenterPrintText", ScrW()/2, ScrH()/2, Color(255,0,0),1,1)
 	end
 
 
